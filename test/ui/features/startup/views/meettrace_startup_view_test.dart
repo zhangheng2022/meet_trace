6ca7dea7d1c1224cbaddeb90eb60f8d8c3112983
zhangheng2022 <962271461@qq.com>
2026-08-27 10:30:41 +0800

    迁移 Forui 0.26 主题依赖

--- test/ui/features/startup/views/meettrace_startup_view_test.dart
+++ test/ui/features/startup/views/meettrace_startup_view_test.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:forui/forui.dart';
 import 'package:meettrace/app/application.dart';
